125b6ee7c75e1bbfb33a4971c56d4f20c7d443a0
Ana204 <anagomes.lucia@hotmail.com>
2021-10-19 22:00:57 -0300

    retrieving the code, getting the token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,33 @@
 
+/**
+ * Receber o code via string
+ * Recuperar o access_token no github
+ * Verificar se o usuario existe no banco de dados 
+ * --- SE existir GERAR TOKEN
+ * --- SENÃO Criar no banco e gera um Token
+ * Retornar o token com as informações do user
+ */
+
+import axios from "axios";
+
 class AuthenticateUserService{
 
     async execute(code: string){
 
+        const url = "https://github.com/login/oauth/access_token";
 
+        const response = await axios.post(url, null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code,
+            },
+            headers: {
+                "Accept": "application/json"
+            }
+        })
 
+        return response.data;
     }
 
 }
