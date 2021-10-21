a5f0910baecfd54723eda40f07b3a821d9f37de6
Ana204 <anagomes.lucia@hotmail.com>
2021-10-20 22:47:31 -0300

    create message

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -2,6 +2,7 @@ import prismaClient from "../prisma";
 
 class CreateMessageService{
     async execute(text: string, user_id: string){
+
         const message = await prismaClient.message.create({
             data:{
                 text,
